441bb3f9964c7ce783f457be69c52dcf1f5dcff7
Raghuram <raghuramakil@gmail.com>
2020-05-29 03:52:58 +0530

    Initial Commit

---
+++ PeopleTenTech_Task/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PeopleTenTech_Task
+//
+//  Created by Raghuram on 29/05/20.
+//  Copyright © 2020 Raghuram. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
